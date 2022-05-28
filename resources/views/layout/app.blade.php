c134159993a533d05d93ce6b996c0e976d871da6
Ahmad Azizi <ahmad.azizi.dev@gmail.com>
2022-05-28 13:06:58 +0300

    Add: model migration seed

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+
+<head>
+    <meta charset="utf-8">
+
+    @yield('head')
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+
+<div class="container">
+    @yield('content')
+</div>
+
+
+<!-- Scripts -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
+
+
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+
+use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [CustomerController::class, 'index'])->name('customer.index');
+Route::post('/customer/store', [CustomerController::class, 'store'])->name('customer.store');
